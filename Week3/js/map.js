acff87a90477e50cdc2a28012c4daea0e95a3263
Ainsleigh Douglas <ainsleigh.douglas@gmail.com>
2021-04-18 11:58:34 -0700

    assignment3

---
+++ Week3/js/map.js
@@ -0,0 +1,166 @@
+
+	var map = L.map('map').setView([37.0902,-95.7129], 4);
+	let data = [
+		{
+			'title':'Olympic Training Center Colorado Springs',
+            'description': 'On of my favorite pools because it was a immeersive environment to training wiht olympians.',
+            'lat': 38.83979182,
+            'lon': -104.79569235,
+            'url': "https://i.pinimg.com/originals/94/7f/86/947f86e5eef27b36034e8e7dda29c695.jpg"
+		},
+
+		{
+			'title':'The Los Angeles Swimming Stadium at Exposition Park',
+			'description': 'One of the first pools I competed at.',
+			'lat': 34.016944,
+			'lon': -118.285,
+            'url': "https://media.timeout.com/images/101588099/image.jpg"
+		},
+		{
+			'title':'San Diego',
+			'description': 'My home town so not super exciting but still some of my fondest memories.',
+			'lat': 32.959903011367,
+			'lon': -117.20161741062,
+            'url': "https://images.squarespace-cdn.com/content/v1/55809453e4b07ca0ea560fa6/1438022174349-L55ITH6QTV1MXK2ICDN5/ke17ZwdGBToddI8pDm48kFWxnDtCdRm2WA9rXcwtIYR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcTSrQkGwCGRqSxozz07hWZrYGYYH8sg4qn8Lpf9k1pYMHPsat2_S1jaQY3SwdyaXg/Cathedral-Catholic_1.jpg?format=750w"
+
+		{
+			'title':'Los Gatos',
+			'description': '',
+			'lat': 37.24193285229,
+			'lon': -121.93191249462
+		},
+		{
+			'title':'Soda Aquatic Center',
+			'description': 'My least favorite pool by far, I have definitely developed a hatred for it.', 
+			'lat': 37.866764253301,
+			'lon': -122.1270831441,
+            'url': "https://www.acalanes.k12.ca.us/cms/lib/CA01001364/Centricity/Domain/27/SodaCenter30.jpg"
+		},
+		{
+			'title':'Oro Valley Aquatic center',
+            'description': 'Gorgeous sunsets from this pool, made the mountains look purple.',
+            'lat': 33.83979182,
+            'lon': -111.9179916,
+            'url': "https://bloximages.chicago2.vip.townnews.com/tucson.com/content/tncms/assets/v3/editorial/e/f5/ef5c529e-9b3d-11e8-a66b-f78785406aa1/5b6b3f1a5d771.image.jpg?resize=1200%2C821"
+		},
+
+		{
+			'title':'University of Richmond',
+			'description': 'Interesting pool, not bad but the smell of chlorine was too intense.',
+			'lat': 37.578355688028,
+			'lon': -77.5407,
+            'url': "https://pbs.twimg.com/media/CRhmmhCW0AAezNJ.jpg:large"
+		},
+		{
+			'title':'Riverside City College',
+			'description': 'Second least favorite pool, I always swam here in the summer, way too hot.',
+			'lat': 33.97146,
+			'lon': -117.38455,
+            'url': "https://riversidecvb.com/sports/wp-content/uploads/sites/3/2015/12/Riverside-Aquatics-Complex.jpg"
+		},
+		{
+			'title':'Santa Clara',
+			'description': 'Nothing special, solid pool.',
+			'lat': 37.3527,
+			'lon': -121.96405,
+            'url': "https://lh3.googleusercontent.com/proxy/1jMf_pK2KmdzzBZCU2X7TgnpfZQvmrWPkAnAWxO90wmXoj46jZzcF6FFDmO8uKtjRXL6I_T-cJ623rOcONdghf2v-ZEUzMIpGq7AXD5p2LJs"
+		},
+		{
+			'title':'Walnut Creek',
+			'description': 'There are rolling green hills here, very pretty for early hours.', 
+			'lat': 37.89905,
+			'lon': -122.05988,
+            'url': "https://wcaquatics.org/wp-content/uploads/2016/06/clarke-swim-1.jpg"
+		},
+		{
+			'title':'Houston',
+			'description': 'Everything is massive here.',
+			'lat': 29.766885576675,
+			'lon': -95.421231426617,
+            'url': "https://bloximages.chicago2.vip.townnews.com/starlocalmedia.com/content/tncms/assets/v3/editorial/5/00/5003a3da-0949-11e4-be13-001a4bcf887a/53c064b47c39b.image.jpg"
+		},
+		{
+			'title':'Boston University',
+			'description': '', 
+			'lat': 42.348402227401,
+			'lon': -71.12058136017,
+            'url': "https://counsilmanhunsaker.com/wp-content/uploads/2005/09/Boston-comp-professional.jpg"
+		},
+		{
+			'title':'Miami University',
+            'description': 'First time placing in the finals at a national championship.',
+            'lat': 39.5087,
+            'lon': -84.7345,
+            'url': "https://i2.wp.com/www.swimmingworldmagazine.com/news/wp-content/uploads/2016/09/miami-university-corwin-nixon-aquatic-center.jpg?fit=1000%2C613&ssl=1"
+		},
+		{
+			'title':'Ohio state',
+			'description': 'Amazing time here, would have been lovely to be a D1 athlete here.',
+			'lat': 40.4173,
+			'lon': -82.9071,
+            'url': "https://ohiostatebuckeyes.com/wp-content/uploads/2018/10/OSUSwimming101218_72-1.jpg"
+		},
+		{
+			'title':'Hearst castle',
+			'description': 'One of the most beautiful pools Ive ever been to, built like a true castle',
+			'lat': 35.6852,
+			'lon': -121.1682,
+            'url': "https://media.architecturaldigest.com/photos/5d1a56ed15c1a800086ea9be/master/w_3000,h_2000,c_limit/GettyImages-1050349422.jpg"
+		},
+		{
+			'title':'Palm Springs',
+			'description': 'First professional show here, absolutely gorgeous pool.',
+			'lat': 33.8303,
+			'lon': -116.5453,
+            'url': "https://static.trip101.com/main_pics/172074/medium.jpg"
+		},
+		{
+			'title':'Buffalo, New York',
+			'description': 'Very first national championship here.', 
+			'lat': 42.8864,
+			'lon': -78.8784,
+            'url': "https://www.tonawanda.ny.us/images/igallery/resized/101-200/Tonawanda_Aquatic_Center_3-109-800-600-80.jpg"
+		}
+		]
+
+			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+			}).addTo(map);
+
+            let myMarkers = L.featureGroup();
+
+			data.forEach(function(item, index){
+                // create marker
+                let marker = L.marker([item.lat,item.lon], {icon: Icon}).bindPopup(`<h3>${item.title}</h3><p>${item.description}</p<br /> <br /> <img src ='${item.url}' width=80% />`)
+            
+                // add marker to featuregroup
+                myMarkers.addLayer(marker)
+            
+                // add data to sidebar with onclick event
+                $('.sidebar').append(`<div class="sidebar-item" onclick="flyToIndex(${index})">${item.title}</div>`)
+            })
+
+            myMarkers.addTo(map)
+
+
+            // define layers
+            let layers = {
+	        "My Markers": myMarkers
+            }
+
+            // add layer control box
+            L.control.layers(null,layers).addTo(map)
+
+
+            // make the map zoom to the extent of markers
+            map.fitBounds(myMarkers.getBounds());
+
+            // function to fly to a location by a given id number
+            function flyToIndex(index){
+                map.flyTo([data[index].lat,data[index].lon],12)
+
+                // open the popup
+                myMarkers.getLayers()[index].openPopup()
+            }
+
+
